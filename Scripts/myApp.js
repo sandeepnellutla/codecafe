7bd887b462e7c9666379175ac1ae7949b3c07308
sandeepnellutla <sandeepnellutla@users.noreply.github.com>
2017-05-17 11:56:16 -0400

    Add files via upload

---
+++ Scripts/myApp.js
@@ -0,0 +1,39 @@
+﻿(function () {
+
+    "use strict";
+
+    var myApp = angular.module('myApp', ['ngRoute']);
+    //config routing
+    myApp.config(function ($routeProvider, $locationProvider) {
+        $locationProvider.hashPrefix('');
+        $routeProvider
+        .when('/', {
+            templateUrl: 'templates/default.html',
+            controller: 'defaultController'
+        })
+        .when('/blog', {
+            templateUrl: 'templates/blog.html',
+            controller: 'blogController'
+        })
+    });
+
+    myApp.controller("homeController", ['$scope', '$location', function ($scope, $location) {
+
+    }]);
+
+    myApp.controller("defaultController", ['$scope', '$location', function ($scope, $location) {
+
+    }]);
+
+    myApp.controller("blogController", function ($scope, $http) {
+        $scope.errorstatus = '';
+
+        $http.get('/blogdata.json')
+           .then(function (response) {
+               $scope.blogs = response.data;
+           })
+
+
+    });
+
+})();
